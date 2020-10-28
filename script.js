ddb3876d371bd1931ddf0731068fee9b8ab2e857
laurasadi <laura.sadinauskaite@gmail.com>
2020-10-28 09:30:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let vardas = "Tomas";
+let alga = 1001;
+let vedes = false;
+
+if(vedes == true && alga > 800)
+//jei zmogus vedes ir jo alga didesne uz 800//
+{
+console.log (vardas+ " paskola gausite");
+}
+
+else if (vedes == false && alga > 1000)
+//jei zmogus nevedes=false ir (&&) jo alga > didesne uz 1000//
+{
+  console.log("sveikinu, kad nevedes, paskola gausite"  +vardas);
+}
+else {
+console.log ("Atvykite susitvarke dokumentus")}
